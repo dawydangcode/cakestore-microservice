2c7646ca5ed5f4f66638c8bb8d3825349c0f1ab3
dawydangcode <122167896+dawydangcode@users.noreply.github.com>
2025-01-10 14:28:52 +0700

    adding homepage-service

---
+++ homepage-service/src/main/java/fit/iuh/edu/vn/homepage_service/controller/HomepageController.java
@@ -0,0 +1,14 @@
+package fit.iuh.edu.vn.homepage_service.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomepageController {
+    @GetMapping("/")
+    public String showHomepage() {
+        return "home/homepage";
+    }
+}
