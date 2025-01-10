72b64d060b5a18ea93baacc42b247e7e02ed907b
dawydangcode <122167896+dawydangcode@users.noreply.github.com>
2025-01-10 13:54:40 +0700

    config api-gateway

--- product-service/src/main/java/fit/iuh/edu/vn/product_service/controller/ProductController.java
+++ product-service/src/main/java/fit/iuh/edu/vn/product_service/controller/ProductController.java
@@ -5,10 +5,12 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/products")
 public class ProductController {
     private final DiscoveryClient discoveryClient;
     private final RestTemplate restTemplate;
@@ -18,7 +20,7 @@ public class ProductController {
         this.restTemplate = new RestTemplate();
     }
 
-    @GetMapping("/users/list")
+    @GetMapping("/list")
     public ModelAndView getUserList() {
         ServiceInstance serviceInstance = discoveryClient.getInstances("user-service").get(0);
         String userListHtml = restTemplate.getForObject(serviceInstance.getUri() + "/users/list", String.class);
