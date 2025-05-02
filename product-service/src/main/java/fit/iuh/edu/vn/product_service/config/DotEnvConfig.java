cf91dcddbc8ad3fc192100cb4c184b61236aad8d
dawydangcode <122167896+dawydangcode@users.noreply.github.com>
2025-05-02 12:32:51 +0700

    update: import aws with DotENV

---
+++ product-service/src/main/java/fit/iuh/edu/vn/product_service/config/DotEnvConfig.java
@@ -0,0 +1,23 @@
+package fit.iuh.edu.vn.product_service.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DotEnvConfig {
+
+    @PostConstruct
+    public void loadDotEnv() {
+        // Tải file .env từ thư mục gốc của dự án
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./.env") // Đường dẫn đến thư mục chứa file .env
+                .ignoreIfMissing() // Bỏ qua nếu không tìm thấy file .env
+                .load();
+
+        // Đặt các biến từ file .env vào môi trường hệ thống
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+    }
+}
